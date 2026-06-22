10712394b8fcf7ea52b070b9d99ae0ad6bcdcc91
Yi Wei Cheah <117268619+yiweicheah@users.noreply.github.com>
2026-06-22 12:00:55 +0800

    Update stage_1_train.py

--- codes/stage_1_train.py
+++ codes/stage_1_train.py
@@ -98,7 +98,7 @@ lora_config = LoraConfig(
     lora_dropout = 0.05,
     bias = "none",
     task_type = "CAUSAL_LM",
-    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj", ]
+    target_modules = ["q_proj", "v_proj", "o_proj" ]
 )
 
 model = get_peft_model(model, lora_config)
